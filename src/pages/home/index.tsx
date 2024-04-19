815c8cde5f0f85f61690e8e191eda3d93960a18c
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-04-19 08:56:57 -0300

    Integrantes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,9 @@
 import Card from '../../componentes/card'
 import Header from '../../componentes/header'
 import Pedro from "../../assets/Pedro.png"
+import Cadu from "../../assets/Cadu.png"
+import Jhemysson from "../../assets/Jhemysson.png"
+import Fernando from "../../assets/Fernando.png"
 import './style.css'
 
 const Home = () => {
@@ -15,38 +18,31 @@ const Home = () => {
                 <div className='bloco2'>
                     <h1>INTEGRANTES</h1>
                     <Card
-                    nome="Pedro"
+                    nome="Pedro Lucca"
                     img={Pedro}
                     email='rm553873@fiap.com.br'
                     rm={553873}
                     />
 
                     <Card
-                    nome="Pedro"
-                    img=''
-                    email='rm553873@fiap.com.br'
-                    rm={553873}
+                    nome="Jhemysson Moura Vieira"
+                    img={Jhemysson}
+                    email='RM552570@fiap.com.br'
+                    rm={552570}
                     />
 
                     <Card
-                    nome="Pedro"
-                    img=''
-                    email='rm553873@fiap.com.br'
-                    rm={553873}
+                    nome="Carlos Eduardo"
+                    img={Cadu}
+                    email='RM553461@fiap.com.br'
+                    rm={553461}
                     />
 
                     <Card
-                    nome="Pedro"
-                    img=''
-                    email='rm553873@fiap.com.br'
-                    rm={553873}
-                    />
-
-                    <Card
-                    nome="Pedro"
-                    img=''
-                    email='rm553873@fiap.com.br'
-                    rm={553873}
+                    nome="Fernando Shinji"
+                    img={Fernando}
+                    email='RM553587@fiap.com.br'
+                    rm={553587}
                     />
                 </div>
             </section>
